bf4b18412d234c2606c68d69e15eff68bbf65e46
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2020-06-25 11:51:09 -0300

    Command handling dinamically

--- src/index.js
+++ src/index.js
@@ -14,3 +14,20 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
+
+client.on("message", (msg) => {
+	if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+	const args = msg.content.slice(prefix.length).split(" ");
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.command.get(command).execute(msg, args, system_manager);
+	} catch (e) {
+		console.error(e);
+		msg.reply(
+			`There was an error trying to execute that command! (${prefix}${command})`
+		);
+	}
+});
